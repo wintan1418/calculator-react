13261e23ddc87d38976de17d896ad3af55e1026c
oluwadare <wintan1418@gmail.com>
2021-07-19 12:49:27 +0100

    complete functionalities for button render

--- src/App.js
+++ src/App.js
@@ -10,3 +10,35 @@ const App = () => {
           <Button symbol = "%"/>
           <Button symbol = "÷" color={buttonColor}/>
         </div>
+        <div className = "row">
+          <Button symbol = "7"/>
+          <Button symbol = "8"/>
+          <Button symbol = "9"/>
+          <Button symbol = "X" color={buttonColor}/>
+        </div>
+        <div className = "row">
+          <Button symbol = "4"/>
+          <Button symbol = "5"/>
+          <Button symbol = "6"/>
+          <Button symbol = " -" color={buttonColor}/>
+        </div>
+        <div className = "row">
+          <Button symbol = "1"/>
+          <Button symbol = "2"/>
+          <Button symbol = "3"/>
+          <Button symbol = "+" color={buttonColor}/>
+        </div>
+        <div className = "row">
+          <Button symbol = "0"/>
+          <Button symbol = "."/>
+          <Button symbol = "=" color={buttonColor}/>
+        </div>
+        <Button symbol="clear" color="red"/>
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
