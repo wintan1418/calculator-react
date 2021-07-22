6b9e3852e459137dd9051d7e93ad499da04b14cc
oluwadare <wintan1418@gmail.com>
2021-07-22 13:41:39 +0100

    add logic functionalities for math operation

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,5 +1,18 @@
 const calculate = ({total = null,next = null,operation = null}, button) => {
   if (button === 'AC') {
     return {total:null,next:null, operation:null};
+  } if (button === '=') {
+  } if (next && operation && total) {
+     return {total: operate(next,operation,total), operation:null,next:null};
+
+  }
+} else if (isOperation (button)) {
+  if (button === '+/-') {
+    if (total) {
+      return { operation, next, total: operate(total, '-1', 'X') };
+    } if (next) {
+      return { next: operate(next, '-1', 'X'), total: null, operation: null };
+    }
+
   }
 }
