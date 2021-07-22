39811be31b6cf173ad30dacff1788d9211ffe6b4
oluwadare <wintan1418@gmail.com>
2021-07-22 13:46:37 +0100

    update logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -13,6 +13,16 @@ const calculate = ({total = null,next = null,operation = null}, button) => {
     } if (next) {
       return { next: operate(next, '-1', 'X'), total: null, operation: null };
     }
+  } else if (button === '%') {
+    if (total) {
+      return { next, operation, total: operate(next, operate(total, '100', '÷'), 'X') };
+    }
+  } else if (total && operation && next) {
+    return {
+      next: operate(next, operation, total),
+      operation: button,
+      total: null,
+    };
 
   }
 }
