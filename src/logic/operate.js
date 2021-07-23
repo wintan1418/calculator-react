a4b27d1522cdfa84929971108087d31705ff1206
oluwadare <wintan1418@gmail.com>
2021-07-23 13:30:32 +0100

    add switch operation for op block

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,3 +3,8 @@ import Big from 'big.js';
 const operate = (num1, num2, op) => {
   const big1 = Big(num1);
   const big2 = Big(num2);
+  let result;
+  switch (op) {
+    case '+':
+      result = big1.plus(big2);
+      break;
