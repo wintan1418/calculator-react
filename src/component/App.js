5de5fa9e1e50fc49fbe5547c98ddabc3f285687b
oluwadare <wintan1418@gmail.com>
2021-07-27 15:38:13 +0100

    complete logic for app.js and adress eslint issue

--- src/component/App.js
+++ src/component/App.js
@@ -1,33 +1,39 @@
+/* eslint-disable react/sort-comp */
 import '../App.css';
+import React from 'react';
+import calculate from '../logic/calculate';
 import Display from './Display';
 import ButtonShape from './ButtonShape';
-import React from 'react';
 
-class App extends React.Component{
- constructor(){
-  super ();
-  this.state = {
-    total:null,
-    total:null,
-    operation:null
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
-  this.handleClick = this.handleClick.bind(this);
- }
- get result() {
-  const { next, total, operation } = this.state;
 
-  return `${next || ''}${total || ''}${operation || ''}`;
-}
-};
+  get result() {
+    const { next, total, operation } = this.state;
 
+    return `${next || ''}${total || ''}${operation || ''}`;
+  }
 
-function App() {
-  return (
-    <>
-      <Display />
-      <ButtonShape />
-    </>
-  );
+  handleClick(name) {
+    this.setState(state => calculate(state, name));
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <Display result={this.result} />
+        <ButtonShape clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
