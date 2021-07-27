5efb14060c5c48edcdaacb08f9212ee6d94e7025
oluwadare <wintan1418@gmail.com>
2021-07-27 13:48:15 +0100

    inititalize constructor

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,14 @@
 import '../App.css';
 import Display from './Display';
 import ButtonShape from './ButtonShape';
+import React from 'react';
+
+class App extends React.Component{
+ constructor()[
+   
+ ]
+};
+
 
 function App() {
   return (
