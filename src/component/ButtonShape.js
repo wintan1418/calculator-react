5a10885a423e240c5ad101872c1aa099222341b6
oluwadare <wintan1418@gmail.com>
2021-07-27 14:46:56 +0100

    add prop function

--- src/component/ButtonShape.js
+++ src/component/ButtonShape.js
@@ -2,21 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-
-const ButtonShape = ({clickHandler}) => (
-  <div className = "button-shape">
+const ButtonShape = ({ clickHandler }) => (
+  <div className="button-shape">
     <div>
       <Button color="E0E0E0" name="AC" clickHandler={clickHandler} />
       <Button color="E0E0E0" name="+/-" clickHandler={clickHandler} />
-      <Button  color="E0E0E0"name="%" clickHandler={clickHandler}/>
-      <Button name="÷" clickHandler={clickHandler}/>
+      <Button color="E0E0E0" name="%" clickHandler={clickHandler} />
+      <Button name="÷" clickHandler={clickHandler} />
     </div>
 
     <div>
-      <Button color="E0E0E0" name="7"clickHandler={clickHandler} />
-      <Button color="E0E0E0" name="8"clickHandler={clickHandler} />
-      <Button color="E0E0E0"name="9"clickHandler={clickHandler} />
-      <Button name="X" clickHandler={clickHandler}/>
+      <Button color="E0E0E0" name="7" clickHandler={clickHandler} />
+      <Button color="E0E0E0" name="8" clickHandler={clickHandler} />
+      <Button color="E0E0E0" name="9" clickHandler={clickHandler} />
+      <Button name="X" clickHandler={clickHandler} />
     </div>
 
     <div>
@@ -27,18 +26,20 @@ const ButtonShape = ({clickHandler}) => (
     </div>
 
     <div>
-      <Button color="E0E0E0" name="1"  clickHandler={clickHandler}/>
-      <Button color="E0E0E0"name="2"  clickHandler={clickHandler}/>
-      <Button color="E0E0E0"name="3" clickHandler={clickHandler} />
+      <Button color="E0E0E0" name="1" clickHandler={clickHandler} />
+      <Button color="E0E0E0" name="2" clickHandler={clickHandler} />
+      <Button color="E0E0E0" name="3" clickHandler={clickHandler} />
       <Button name="+" clickHandler={clickHandler} />
     </div>
 
     <div>
-      <Button color="E0E0E0"name="0" clickHandler={clickHandler} />
-      <Button color="E0E0E0"name="."  clickHandler={clickHandler}/>
+      <Button color="E0E0E0" name="0" clickHandler={clickHandler} />
+      <Button color="E0E0E0" name="." clickHandler={clickHandler} />
       <Button name="=" clickHandler={clickHandler} />
     </div>
-  </>
+  </div>
 );
-
+ButtonShape.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
+};
 export default ButtonShape;
