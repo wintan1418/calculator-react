ebcb6e4e88e6b801f982e426e26acb188045d6b0
oluwadare <wintan1418@gmail.com>
2021-07-28 16:22:24 +0100

    add cloickhanler methods for button

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import './Button.css';
 
-const Button = ({ name }) => (
-  <button type="button">{name}</button>
+const Button = ({
+  name, color, wide, clickHandler,
+}) => (
+  <button
+    type="button"
+    style={{ width: wide ? '50%' : '25%', background: color }}
+    onClick={clickHandler.bind(null, name)}
+
+  >
+    {name}
+  </button>
 );
 
 Button.propTypes = {
