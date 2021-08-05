5e5a68e747e1003e4184147d19daa83f60785a25
oluwadare <wintan1418@gmail.com>
2021-08-05 11:27:36 +0100

    add stylings and update linkings

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,15 +4,18 @@ import './App.css';
 const Home = () => (
   <>
     <div className="calculator">
-      <h2 className="title">Welcome to Math-Magicians</h2>
+      <h2 className="title">Welcome to Wintan calculator</h2>
       <div className="text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui o!cia deserunt mollit anim id est laborum.
+        There are 26 letters of the English alphabet, but there are 44 sounds of English.
+        Kids learn letters, but they also need to learn phonemes and graphemes. And once they’ve
+        gotten that, they need to segment,
+        blend, and manipulate sounds. Let’s break all that down and look at some ways
+        to teach the 44 sounds of English.Phonemes are basic units of sounds.
+        There are 44 phonemes that can be blended to form additional sounds.Graphemes are
+        the way we write phonemes. You can have a one letter grapheme or
+        multi-letter grapheme. For example, digraphs are two-letter graphemes that represent
+        one phoneme or two letters that represent one sound. You can also have three- or
+        four-letter graphemes.
       </div>
     </div>
   </>
--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -8,8 +8,9 @@ const Quote = () => (
       <h1 className="quote">Quote of the day</h1>
       <p className="day-quote">
         <q>
-          Without mathematics, Everything around you is numbers.
-          <span>Albert Einstein</span>
+          Without mathematics, there’s nothing you can do. Everything around you is mathematics.
+          Everything around you is numbers..
+          <span>– Shakuntala Devi</span>
         </q>
       </p>
 
